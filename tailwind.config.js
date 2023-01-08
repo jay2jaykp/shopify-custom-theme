06a553b7d785db978864f4799f70cfd6b75bdd78
jay.patel5 <jay.patel5@loblaw.ca>
2023-01-08 13:18:47 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+      './layout/*.liquid',
+      './templates/*.liquid',
+      './sections/*.liquid',
+      './snippets/*.liquid'
+    ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+  ]
+}
